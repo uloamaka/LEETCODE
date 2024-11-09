32fdea4543b89b2563eec0fd451b2f11d224795a
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2024-11-09 03:44:37 +0100

    [1661] [Time Beats: 90.45%] - LeetPush

--- 1661-Average-Time-of-Process-per-Machine.sql
+++ 1661-Average-Time-of-Process-per-Machine.sql
@@ -3,19 +3,9 @@
 -- A1 - END 
 -- A0 - START
 SELECT 
-    machine_id,
-    round(avg(subQ.processing_time)::numeric, 3) AS processing_time
-FROM (    
-SELECT 
-    A1.machine_id, 
-    A1.process_id,
-    A1.timestamp AS A1,
-    A0.timestamp AS A0,
-    (A1.timestamp - A0.timestamp) AS processing_time
+    A1.machine_id,
+    round(avg(A1.timestamp - A0.timestamp)::numeric, 3) AS processing_time
 FROM Activity A1
 INNER JOIN Activity A0 ON A1.machine_id = A0.machine_id 
-AND A1.process_id = A0.process_id
-AND A1.activity_type = 'end'
-AND A0.activity_type = 'start'
-) subQ
-GROUP BY subQ.machine_id;
+WHERE A0.activity_type = 'start' AND A1.activity_type = 'end'
+GROUP BY A1.machine_id;
