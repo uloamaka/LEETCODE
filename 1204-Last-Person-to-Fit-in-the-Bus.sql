e945c3d4f743a5fe41c1bff5a2804d58f1bc0674
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2024-12-01 14:41:55 +0100

    [1204] [Time Beats: 75.41%] - LeetPush

---
+++ 1204-Last-Person-to-Fit-in-the-Bus.sql
@@ -0,0 +1,23 @@
+-- Write your PostgreSQL query statement below
+-- Rank with the turns colummn 
+-- Sum up the weight column, to get up to 1000
+-- select the name of the passenger that made the total weight to 1000
+
+WITH CTE_ORD AS (
+    SELECT 
+        RANK() OVER (
+            ORDER BY turn
+        ) AS turn,
+        person_id,
+        person_name,
+        weight,
+        SUM(weight) OVER (ORDER BY turn) AS total_weight
+    FROM 
+    Queue)
+    
+    SELECT 
+        LAST_VALUE(person_name) OVER (ORDER BY total_weight DESC) AS person_name
+    FROM  CTE_ORD
+    WHERE total_weight::NUMERIC <= 1000
+    LIMIT 1
+;
