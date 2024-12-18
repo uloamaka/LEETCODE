adddad0ac0f237a5e337705a3b62f59da24a295b
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2024-12-18 22:37:43 +0100

    [1193] [Time Beats: 10.16%] - LeetPush

--- 1193-Monthly-Transactions-I.sql
+++ 1193-Monthly-Transactions-I.sql
@@ -1,10 +1,11 @@
--- Write your PostgreSQL query statement below
+# Write your MySQL query statement below
+-- GROUP BY MONTH AND COUNTRY, 
 SELECT 
-    to_char(trans_date, 'YYYY-MM') AS month,
+    DATE_FORMAT( trans_date, '%Y-%m' ) month,
     country,
-    COUNT(id) AS trans_count,
-    SUM(CASE WHEN Transactions.state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
-    SUM(amount) AS trans_total_amount,
-    SUM(CASE WHEN Transactions.state = 'approved' THEN Transactions.amount ELSE 0 END) AS approved_total_amount
-FROM Transactions
-GROUP BY month, country;
+    COUNT(state) trans_count,
+    SUM(state = 'approved') approved_count,
+    SUM(amount) trans_total_amount,
+    SUM((state = 'approved') * amount)  approved_total_amount
+FROM Transactions 
+GROUP BY month, country
