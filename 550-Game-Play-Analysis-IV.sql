c127e82407ce4b455db19a0330e443192c165cef
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-01-04 13:14:47 +0100

    [550] [Time Beats: 59.43%] - LeetPush

--- 550-Game-Play-Analysis-IV.sql
+++ 550-Game-Play-Analysis-IV.sql
@@ -1,25 +1,11 @@
--- Write your PostgreSQL query statement below
-
-
--- WITH CTE_TEST1 AS (SELECT 
---     MIN(event_date) AS  first_login,
---     MIN(event_date) + 1  AS  next_day_after_first_login_date,
---     player_id
--- FROM Activity
--- GROUP BY player_id) 
--- SELECT 
---     ROUND(1.0 * COUNT(A.player_id) / COUNT(CTE_TEST1.player_id), 2) AS fraction
--- FROM Activity A 
--- RIGHT JOIN CTE_TEST1 
---     ON A.player_id = CTE_TEST1.player_id AND A.event_date = CTE_TEST1.next_day_after_first_login_date;
-
-SELECT ROUND(
-    COUNT(player_id)::NUMERIC / 
-    (SELECT COUNT(DISTINCT player_id)
-    FROM Activity), 2) AS fraction
-FROM Activity
-WHERE (player_id, event_date) IN (
-    SELECT player_id, MIN(event_date) + 1
-    FROM Activity
-    GROUP BY player_id
+# Write your MySQL query statement below
+SELECT 
+    ROUND(COUNT(A1.player_id) / 
+    (SELECT COUNT(DISTINCT A3.player_id)
+    FROM Activity A3), 2) AS fraction
+FROM Activity A1
+WHERE (player_id, DATE_SUB(A1.event_date, INTERVAL 1 DAY)) IN (
+    SELECT A2.player_id, MIN(A2.event_date) 
+    FROM Activity A2
+    GROUP BY A2.player_id
 )
