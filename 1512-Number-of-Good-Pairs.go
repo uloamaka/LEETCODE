a0107927f8071671f012c7d783ea1bc1bef6bb9f
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-01-28 10:17:04 +0100

    Update 1512-Number-of-Good-Pairs.go

--- 1512-Number-of-Good-Pairs.go
+++ 1512-Number-of-Good-Pairs.go
@@ -10,7 +10,7 @@ func numIdenticalPairs(nums []int) int {
     return sum
 }
     // well I didn't understand the question
-    // but here is what i got for the discussion segment
+    // but here is what i got from the discussion segment
     // count each unique numbers in a given array as N, 
     // then applly the formula N * (N - 1) / 2 
     // then sum the total count of all N
