59913663e008a8c1d440b997cbdc8c510837cf83
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-01-31 18:05:24 +0100

    [2044] [Time Beats: 88.46%] [Memory Beats: 38.46%] - LeetPush

--- 2044-Count-Number-of-Maximum-Bitwise-OR-Subsets.go
+++ 2044-Count-Number-of-Maximum-Bitwise-OR-Subsets.go
@@ -1,24 +1,19 @@
 func countMaxOrSubsets(nums []int) int {
-    maxOR := 0
-    for _, x := range nums {
-        maxOR |= x
-    }
+	var count int = 0
+	maxOR := 0
+	for _, num := range nums {
+		maxOR |= num
+	}
+	backtrack(nums, 0, maxOR, 0, &count)
+	return count
+}
 
-    count := 0
+func backtrack(nums []int, index int, maxOR int, currentOR int, count *int) {
+	if currentOR == maxOR {
+		*count++
+	}
 
-    var backtrack func(int, int)
-    backtrack = func(i, currOR int) {
-        if i == len(nums) {
-            if currOR == maxOR {
-                count++
-            }
-            return
-        }
-
-        backtrack(i+1, currOR|nums[i])
-        backtrack(i+1, currOR)
-    }
-
-    backtrack(0, 0)
-    return count
+	for i := index; i < len(nums); i++ {
+		backtrack(nums, i+1, maxOR, currentOR|nums[i], count)
+	}
 }
