4b845e56795c96da27edd64576c7d7831056a810
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-02-08 23:47:09 +0100

    [3264] [Time Beats: 100.00%] [Memory Beats: 75.12%] - LeetPush

---
+++ Array_Problems_and_solutions/3264-Final-Array-State-After-K-Multiplication-Operations-I.go
@@ -0,0 +1,25 @@
+func getFinalState(nums []int, k int, multiplier int) []int {
+    for i := 0; i < k; i ++ {
+        index, x := minIntSlice(nums)
+        nums[index] = x * multiplier
+    }
+    return nums
+}
+
+
+func minIntSlice(v []int) (minIndex, minValue int) {
+    if len(v) == 0 {
+        return 0, 0
+    }
+    
+    minValue = v[0]
+    minIndex = 0
+    
+    for i, e := range v {
+        if e < minValue {
+            minValue = e
+            minIndex = i
+        }
+    }
+    return minIndex, minValue
+}
