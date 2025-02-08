f492a6820d6cd877f3909b8f1a5376264c3f1b52
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-02-08 22:38:06 +0100

    [2824] [Time Beats: 100.00%] [Memory Beats: 76.42%] - LeetPush

--- 2824-Count-Pairs-Whose-Sum-is-Less-than-Target.go
+++ 2824-Count-Pairs-Whose-Sum-is-Less-than-Target.go
@@ -1,8 +1,8 @@
 func countPairs(nums []int, target int) int {
     count := 0
-    sort.Ints(nums)
     left := 0
     right := len(nums) - 1
+    sort.Ints(nums)
     for left < right {  
         if nums[right] + nums[left] < target {
             count += right - left
