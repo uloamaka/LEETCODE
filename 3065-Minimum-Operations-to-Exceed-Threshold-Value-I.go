e92521adab641ba3a836abdc81399b6597ebe78e
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-02-19 15:52:51 +0100

    [3065] [Time Beats: 100.00%] [Memory Beats: 93.33%] - LeetPush

--- 3065-Minimum-Operations-to-Exceed-Threshold-Value-I.go
+++ 3065-Minimum-Operations-to-Exceed-Threshold-Value-I.go
@@ -1,16 +1,9 @@
 func minOperations(nums []int, k int) int {
-    ans := 0
     sort.Ints(nums)
     for i, val := range nums {
         if val >= k {
-            ans = i
-            break
+            return i
         }
     }
-    return ans
+    return 0
 }
-// Algorithm
-// sort the nums array
-// loop through the nums 
-// range through the slice get the first index where the value is >= to k
-// then return that index
