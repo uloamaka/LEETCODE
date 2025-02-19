b26832341273a8969844d3987781e957c52bc061
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-02-19 15:26:05 +0100

    [3285] [Time Beats: 100.00%] [Memory Beats: 52.00%] - LeetPush

---
+++ 3285-Find-Indices-of-Stable-Mountains.go
@@ -0,0 +1,9 @@
+func stableMountains(height []int, threshold int) []int {
+    ans := []int{}
+    for i := 1; i < len(height); i++ {
+        if height[i - 1] > threshold {
+            ans = append(ans, i)
+        }
+    }
+    return ans
+}
