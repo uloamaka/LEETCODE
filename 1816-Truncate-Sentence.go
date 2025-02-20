5d253a6f60b2022eca4f983a05a7b52bab447cf5
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-02-20 15:06:28 +0100

    [1816] [Time Beats: 100.00%] [Memory Beats: 70.59%] - LeetPush

--- 1816-Truncate-Sentence.go
+++ 1816-Truncate-Sentence.go
@@ -1,9 +1,24 @@
-func truncateSentence(s string, k int) string {
-    slcStr := strings.Split(s, " ")
+// func truncateSentence(s string, k int) string {
+//     slcStr := strings.Split(s, " ")
 
-    if k < len(slcStr) {
-        return strings.Join(slcStr[:k], " ")
-    } else {
-        return s
-    }
+//     if k < len(slcStr) {
+//         return strings.Join(slcStr[:k], " ")
+//     } else {
+//         return s
+//     }
+// }
+
+func truncateSentence(s string, k int) string {
+    space := rune(' ')
+    spaceCnt := 0
+
+    for idx, char := range s {
+        if char == space {
+            spaceCnt++
+        }
+        if k == spaceCnt {
+            return s[0 : idx]
+        }
+    } 
+    return s
 }
