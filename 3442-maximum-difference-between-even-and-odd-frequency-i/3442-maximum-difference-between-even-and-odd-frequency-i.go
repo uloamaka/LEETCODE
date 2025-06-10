180ae158dcbe4b03ac488e81312f015026a619bb
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-06-11 00:08:25 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.go
+++ 3442-maximum-difference-between-even-and-odd-frequency-i/3442-maximum-difference-between-even-and-odd-frequency-i.go
@@ -4,28 +4,21 @@ func maxDifference(s string) int {
 		xMaps[xter]++ 
 	}
 
-    // n := len(xMaps)
    	sm := rankByXterCount(xMaps)
 
 	var (
-		maxEven, minEven int
-		maxOdd, minOdd   int
-		foundEven        bool
-		foundOdd         bool
+		minEven int
+		maxOdd int
+		foundEven bool
+		foundOdd bool
 	)
 
-	// Initialize minEven/minOdd with max possible, maxEven/maxOdd with min possible
-	minEven = 1 << 31    // Start with a very large number
-	minOdd = 1 << 31     // Start with a very large number
-	maxEven = -1 << 31   // Start with a very small number
-	maxOdd = -1 << 31    // Start with a very small number
+	minEven = 1 << 31    
+	maxOdd = -1 << 31    
 
 	for _, num := range sm {
 		if checkEven(num) {
 			foundEven = true
-			if num > maxEven {
-				maxEven = num
-			}
 			if num < minEven {
 				minEven = num
 			}
@@ -34,24 +27,11 @@ func maxDifference(s string) int {
 			if num > maxOdd {
 				maxOdd = num
 			}
-			if num < minOdd {
-				minOdd = num
-			}
 		}
 	}
 
-	if !foundEven || !foundOdd {
-		return 0 // No even or odd frequencies
-	}
-
-	// The maximum difference can be either:
-	// (maxEven - minOdd) or (maxOdd - minEven)
-	// diff1 := maxEven - minOdd
 	diff2 := maxOdd - minEven
 
-	// if diff1 > diff2 {
-	// 	return diff1
-	// }
 	return diff2
 }
 
