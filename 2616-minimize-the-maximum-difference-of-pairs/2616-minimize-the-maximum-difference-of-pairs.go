c270e4d5eaccbc8c0245178757e2815a79809d97
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-06-13 22:54:27 +0100

    Time: 12 ms (76.92%), Space: 13.5 MB (15.38%) - LeetHub

---
+++ 2616-minimize-the-maximum-difference-of-pairs/2616-minimize-the-maximum-difference-of-pairs.go
@@ -0,0 +1,31 @@
+func minimizeMax(nums []int, p int) int {
+    sort.Ints(nums)
+
+    left := 0
+    right := nums[len(nums)-1] - nums[0]
+    result := right
+    for left <= right {
+        mid := left + (right - left) / 2
+        if countValidPairs(nums, mid) >= p {
+            result = mid
+            right = mid - 1
+        } else {
+            left = mid + 1
+        }
+    }
+    return result
+}
+
+func countValidPairs(nums []int, mid int) int {
+    count := 0
+	i := 0
+	for i < len(nums)-1 {
+		if nums[i+1]-nums[i] <= mid {
+			count++
+			i += 2 // skip next element as it's already paired
+		} else {
+			i++
+		}
+	}
+	return count
+}
