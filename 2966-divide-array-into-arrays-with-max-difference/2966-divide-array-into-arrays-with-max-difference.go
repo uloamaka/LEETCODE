9bee447ab00b6c6d57647a88925d5fc57c28e54b
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-06-18 12:48:34 +0100

    Time: 36 ms (61.29%), Space: 12.1 MB (51.61%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.go
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.go
@@ -1,14 +1,11 @@
 func divideArray(nums []int, k int) [][]int {
     sort.Ints(nums)
-    
+
     n := len(nums)
     ans := [][]int{}
-    left, right := 0, 3
     for i := 0; i < n; i += 3 {
-        if nums[right-1] - nums[left] <= k {
-            ans = append(ans, nums[left:right])
-            left = right
-            right = right + 3
+        if nums[i+2] - nums[i] <= k {
+            ans = append(ans, nums[i:i+3])
         } else {
             return [][]int{}
         }
