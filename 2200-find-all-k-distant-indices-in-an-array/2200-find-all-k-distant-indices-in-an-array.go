a654132a920195abc0c5e810a02837919c9834fb
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-06-24 06:00:45 +0100

    Time: 60 ms (5.26%), Space: 6.7 MB (7.89%) - LeetHub

--- 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.go
+++ 2200-find-all-k-distant-indices-in-an-array/2200-find-all-k-distant-indices-in-an-array.go
@@ -1,28 +1,35 @@
 func findKDistantIndices(nums []int, key int, k int) []int {
     ans := []int{}
     n := len(nums)
-    m := make(map[int]int)
-    for idx, num := range nums {
+    m := make(map[int]bool)
+    for j, num := range nums {
         if num == key {
-            for i := idx; i < n; i++ {
-                l := max(0, i-k)
-                r := min(n-1, i+k)
-                if math.Abs(float64(l - idx)) <= float64(k) {
-                    if _, ok := m[l]; !ok {
-                        m[l] = l
-                    }
-                }
-                if math.Abs(float64(r - idx)) <= float64(k) {
-                    if _, ok := m[r]; !ok {
-                        m[r] = r
-                    }
-                }
-            }
+            l := max(0, j-k)
+            r := min(n-1, j+k)
+            for i := l; i <= r; i++ {
+				m[i] = true
+			}
+    
         }
     }
-    for _, val := range m {
+    
+    for val := range m {
         ans = append(ans, val)
     }
+
     sort.Ints(ans)
     return ans
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+func min(a, b int) int {
+    if a < b {
+        return a
+    }
+    return b
 }
