425edd17067ecde87c88548402655a79c556094e
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-06-30 03:55:39 +0100

    Time: 29 ms (8.45%), Space: 9.1 MB (45.07%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.go
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.go
@@ -3,11 +3,12 @@ func findLHS(nums []int) int {
     for _, num := range nums {
         freq[num]++
     }
-
+    
     keySli := []int{}
     for key, _ := range freq {
         keySli = append(keySli, key)
     }
+    sort.Ints(keySli)
 
     highestFreqCnt := 0
     for i := 1; i < len(keySli); i++ {
@@ -18,6 +19,9 @@ func findLHS(nums []int) int {
             }
         }
     }
-
     return highestFreqCnt
 }
+
+    // get the freq map of nums
+    // get the highestFreqCnt, i.e the sum of cnt of 2 subsequent nums in the freq slice
+    // return tthe cnt
