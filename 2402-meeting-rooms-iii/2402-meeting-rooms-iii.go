b9af676fb18f06af34829b6dd15642bbbf6d3f58
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-07-11 11:09:40 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.go
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.go
@@ -1,52 +1,71 @@
+type Room struct {
+	id        int
+	available int
+}
+
+type RoomHeap []Room
+
+func (h RoomHeap) Len() int           { return len(h) }
+func (h RoomHeap) Less(i, j int) bool {
+	if h[i].available == h[j].available {
+		return h[i].id < h[j].id
+	}
+	return h[i].available < h[j].available
+}
+func (h RoomHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *RoomHeap) Push(x interface{}) { *h = append(*h, x.(Room)) }
+func (h *RoomHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	room := old[n-1]
+	*h = old[:n-1]
+	return room
+}
+
 func mostBooked(n int, meetings [][]int) int {
-    sort.Slice(meetings, func(i, j int) bool {
+	sort.Slice(meetings, func(i, j int) bool {
 		return meetings[i][0] < meetings[j][0]
 	})
-    meeting_count := make([]int, n)
-	room_availability_time := make(map[int]int, n)
+
+	meetingCount := make([]int, n)
+
+	freeRooms := &RoomHeap{}
+	occupiedRooms := &RoomHeap{}
+
 	for i := 0; i < n; i++ {
-		room_availability_time[i] = 0
+		heap.Push(freeRooms, Room{id: i, available: 0})
 	}
 
 	for _, meeting := range meetings {
 		start, end := meeting[0], meeting[1]
 		duration := end - start
-		assigned := false
 
-		// Try to assign meeting to a free room
-		for i := 0; i < n; i++ {
-			if room_availability_time[i] <= start {
-				room_availability_time[i] = end
-				meeting_count[i]++
-				assigned = true
-				break
-			}
+		// Free up rooms that become available by current start time
+		for occupiedRooms.Len() > 0 && (*occupiedRooms)[0].available <= start {
+			room := heap.Pop(occupiedRooms).(Room)
+			heap.Push(freeRooms, Room{id: room.id, available: room.available})
 		}
 
-		// If no room was free, assign to the one with earliest availability
-		if !assigned {
-			earliestTime := int(1e18) // some large number
-			chosenRoom := -1
-			for i := 0; i < n; i++ {
-				if room_availability_time[i] < earliestTime {
-					earliestTime = room_availability_time[i]
-					chosenRoom = i
-				}
-			}
-			room_availability_time[chosenRoom] += duration
-			meeting_count[chosenRoom]++
+		if freeRooms.Len() > 0 {
+			room := heap.Pop(freeRooms).(Room)
+			heap.Push(occupiedRooms, Room{id: room.id, available: end})
+			meetingCount[room.id]++
+		} else {
+			room := heap.Pop(occupiedRooms).(Room)
+			newEnd := room.available + duration
+			heap.Push(occupiedRooms, Room{id: room.id, available: newEnd})
+			meetingCount[room.id]++
 		}
 	}
 
-	// Find the room with the most meetings (prefer smaller index in case of tie)
-	maxCount := -1
-	resultRoom := -1
+	// Get the room with the most meetings (smallest index in case of tie)
+	maxMeetings, result := -1, -1
 	for i := 0; i < n; i++ {
-		if meeting_count[i] > maxCount || (meeting_count[i] == maxCount && i < resultRoom) {
-			maxCount = meeting_count[i]
-			resultRoom = i
+		if meetingCount[i] > maxMeetings || (meetingCount[i] == maxMeetings && i < result) {
+			maxMeetings = meetingCount[i]
+			result = i
 		}
 	}
 
-	return resultRoom
+	return result
 }
