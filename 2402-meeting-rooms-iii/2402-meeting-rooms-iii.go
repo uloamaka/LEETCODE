df3947aa97a36ed000eaed2a5ce57bcb49ade7db
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-07-11 11:10:27 +0100

    Time: 70 ms (46.88%), Space: 19.4 MB (43.75%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.go
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.go
@@ -3,23 +3,40 @@ type Room struct {
 	available int
 }
 
-type RoomHeap []Room
+type OccupiedHeap []Room
+type FreeHeap []int
 
-func (h RoomHeap) Len() int           { return len(h) }
-func (h RoomHeap) Less(i, j int) bool {
+func (h OccupiedHeap) Len() int { return len(h) }
+func (h OccupiedHeap) Less(i, j int) bool {
 	if h[i].available == h[j].available {
 		return h[i].id < h[j].id
 	}
 	return h[i].available < h[j].available
 }
-func (h RoomHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *RoomHeap) Push(x interface{}) { *h = append(*h, x.(Room)) }
-func (h *RoomHeap) Pop() interface{} {
+func (h OccupiedHeap) Swap(i, j int) { h[i], h[j] = h[j], h[i] }
+func (h *OccupiedHeap) Push(x interface{}) {
+	*h = append(*h, x.(Room))
+}
+func (h *OccupiedHeap) Pop() interface{} {
 	old := *h
 	n := len(old)
 	room := old[n-1]
 	*h = old[:n-1]
 	return room
+}
+
+func (h FreeHeap) Len() int           { return len(h) }
+func (h FreeHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h FreeHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+func (h *FreeHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+func (h *FreeHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	roomID := old[n-1]
+	*h = old[:n-1]
+	return roomID
 }
 
 func mostBooked(n int, meetings [][]int) int {
@@ -28,29 +45,29 @@ func mostBooked(n int, meetings [][]int) int {
 	})
 
 	meetingCount := make([]int, n)
-
-	freeRooms := &RoomHeap{}
-	occupiedRooms := &RoomHeap{}
+	freeRooms := &FreeHeap{}
+	occupiedRooms := &OccupiedHeap{}
 
 	for i := 0; i < n; i++ {
-		heap.Push(freeRooms, Room{id: i, available: 0})
+		heap.Push(freeRooms, i)
 	}
 
 	for _, meeting := range meetings {
 		start, end := meeting[0], meeting[1]
 		duration := end - start
 
-		// Free up rooms that become available by current start time
+		// Free up rooms available by start time
 		for occupiedRooms.Len() > 0 && (*occupiedRooms)[0].available <= start {
 			room := heap.Pop(occupiedRooms).(Room)
-			heap.Push(freeRooms, Room{id: room.id, available: room.available})
+			heap.Push(freeRooms, room.id)
 		}
 
 		if freeRooms.Len() > 0 {
-			room := heap.Pop(freeRooms).(Room)
-			heap.Push(occupiedRooms, Room{id: room.id, available: end})
-			meetingCount[room.id]++
+			roomID := heap.Pop(freeRooms).(int)
+			heap.Push(occupiedRooms, Room{id: roomID, available: end})
+			meetingCount[roomID]++
 		} else {
+			// All rooms busy, wait for the earliest available room
 			room := heap.Pop(occupiedRooms).(Room)
 			newEnd := room.available + duration
 			heap.Push(occupiedRooms, Room{id: room.id, available: newEnd})
@@ -58,11 +75,11 @@ func mostBooked(n int, meetings [][]int) int {
 		}
 	}
 
-	// Get the room with the most meetings (smallest index in case of tie)
-	maxMeetings, result := -1, -1
+	// Find room with max meetings (break tie by smaller index)
+	maxCount, result := -1, -1
 	for i := 0; i < n; i++ {
-		if meetingCount[i] > maxMeetings || (meetingCount[i] == maxMeetings && i < result) {
-			maxMeetings = meetingCount[i]
+		if meetingCount[i] > maxCount || (meetingCount[i] == maxCount && i < result) {
+			maxCount = meetingCount[i]
 			result = i
 		}
 	}
