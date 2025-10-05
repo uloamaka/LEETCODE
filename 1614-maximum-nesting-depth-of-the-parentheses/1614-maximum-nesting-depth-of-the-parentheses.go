72a2fc3e98c62d1a27867727a49f2d2f2f0d562e
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-10-05 02:20:37 +0100

    Time: 0 ms (100%), Space: 3.9 MB (41.67%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.go
@@ -0,0 +1,21 @@
+func maxDepth(s string) int {
+    stack := []rune{}
+
+    maxSeen := 0
+    for _, rn := range s {
+
+        if rn == '(' {
+            stack  = append(stack, rn)
+        }
+        
+        if rn == ')' {
+            if stack[len(stack)-1] == '(' {
+                if len(stack) > maxSeen {
+                    maxSeen = len(stack)
+                }
+                stack = stack[:len(stack)-1]
+            }
+        }
+    }
+    return maxSeen
+}
