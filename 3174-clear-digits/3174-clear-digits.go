016bb8581738754443ce41360050b6d91461b378
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-10-06 22:14:50 +0100

    Time: 0 ms (100%), Space: 4.2 MB (73.33%) - LeetHub

--- 3174-clear-digits/3174-clear-digits.go
+++ 3174-clear-digits/3174-clear-digits.go
@@ -2,7 +2,7 @@ func clearDigits(s string) string {
     stack := []byte{}
     
     for i:= 0; i < len(s);  i++ {
-        if !unicode.IsDigit(rune(s[i])) {
+        if s[i] < '0' || s[i] > '9' {
             stack = append(stack, s[i])
         } else {
             stack = stack[:len(stack)-1]
