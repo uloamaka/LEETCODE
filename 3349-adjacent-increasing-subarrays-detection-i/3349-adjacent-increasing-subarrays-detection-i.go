11fa820fbac7fe9e65f55a977e530c6fb98d8e21
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-10-14 23:26:57 +0100

    Time: 11 ms (33.33%), Space: 6 MB (100%) - LeetHub

--- 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.go
+++ 3349-adjacent-increasing-subarrays-detection-i/3349-adjacent-increasing-subarrays-detection-i.go
@@ -1,37 +1,15 @@
 func hasIncreasingSubarrays(nums []int, k int) bool {
-    n := len(nums)
-    l := 0
-    ans1, ans2 := false, false
-    for l <= n-2*k {
-        r := l+k
-
-        fseen1 := -1000
-        for _, val := range nums[l:l+k] {
-            if fseen1 >= val {
-                ans1 = false
-                break
-            } else {
-                fseen1 = val
-                ans1 = true
-            }
-        }
-        
-        fseen2 := -1000
-        for _, val := range nums[r:r+k] {
-            if fseen2 >= val {
-                ans2 = false
-                break
-            } else {
-                fseen2 = val
-                ans2 = true
-            }
-        }
-
-        if ans1 == true && ans2 == true {
-            return true
-        }
-
-        l++
-    }
-    return false
+	n := len(nums)
+	cnt, precnt, ans := 1, 0, 0
+	for i := 1; i < n; i++ {
+		if nums[i] > nums[i-1] {
+			cnt++
+		} else {
+			precnt = cnt
+			cnt = 1
+		}
+		ans = max(ans, min(precnt, cnt))
+		ans = max(ans, cnt/2)
+	}
+	return ans >= k
 }
