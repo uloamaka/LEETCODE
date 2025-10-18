dc65a2a8e7c77264d42356207020a71045a28967
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-10-18 16:59:31 +0100

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.go
+++ 1381-design-a-stack-with-increment-operation/1381-design-a-stack-with-increment-operation.go
@@ -1,35 +1,40 @@
 type CustomStack struct {
     stack []int
+    maxSize int
 }
 
 
 func Constructor(maxSize int) CustomStack {
-    return CustomStack{ stack: make([]int, 0, maxSize)}
+    return CustomStack{ 
+        stack: make([]int, 0, maxSize), 
+        maxSize: maxSize,
+    }
 }
 
 
 func (this *CustomStack) Push(x int)  {
-    if len(this.stack) < cap(this.stack) {
+    if len(this.stack) < this.maxSize {
         this.stack = append(this.stack, x)
     }  
 }
 
 
 func (this *CustomStack) Pop() int {
-    if len(this.stack) ==  0 {
+    n := len(this.stack)
+    if n ==  0 {
         return -1
     }
-    elem := this.stack[len(this.stack)-1]
-    this.stack = this.stack[:len(this.stack)-1]
+    elem := this.stack[n-1]
+    this.stack = this.stack[:n-1]
     return elem
 }
 
 
 func (this *CustomStack) Increment(k int, val int)  {
-    m := len(this.stack)
+    m := this.maxSize
     for i := 0; i < k; i++ {
         if m > 0 {
-            this.stack[i] = this.stack[i] + val
+            this.stack[i] += val
             m--
         } else {
             break
