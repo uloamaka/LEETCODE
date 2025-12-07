d2a50c5906c7b96e9787c3efac527e7897aaa39a
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-12-07 23:11:32 +0100

    Time: 0 ms (100%), Space: 3.9 MB (100%) - LeetHub

--- 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.go
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.go
@@ -3,7 +3,7 @@ func minAddToMakeValid(s string) int {
     for _, str := range s {
         if str == '(' {
             stack = append(stack, str)
-        } else if len(stack) >= 1 && stack[len(stack)-1] == '('{
+        } else if len(stack) > 0 && stack[len(stack)-1] == '('{
             stack = stack[:len(stack)-1]
             continue
         } else {
