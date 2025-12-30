29d92ad814f9a72dc9be1388d9a00b24d6f3599c
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2025-12-30 13:24:33 +0100

    Time: 0 ms (100%), Space: 8.1 MB (31.07%) - LeetHub

--- 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.go
+++ 3746-minimum-string-length-after-balanced-removals/3746-minimum-string-length-after-balanced-removals.go
@@ -1,17 +1,36 @@
+// func minLengthAfterRemovals(s string) int {
+//     stack := []rune{}
+
+//     for _, rn := range s {
+//         if len(stack) > 0 && rn == 'b' && stack[len(stack)-1] == 'a' {
+//             stack = stack[:len(stack)-1]
+//             continue
+//         }
+
+//         if len(stack) > 0 && rn == 'a' && stack[len(stack)-1] == 'b' {
+//             stack = stack[:len(stack)-1]
+//             continue
+//         }
+//         stack = append(stack, rn)
+//     }
+//     return len(stack)
+// }
+
 func minLengthAfterRemovals(s string) int {
-    stack := []rune{}
-
+    cnt := 0 
     for _, rn := range s {
-        if len(stack) > 0 && rn == 'b' && stack[len(stack)-1] == 'a' {
-            stack = stack[:len(stack)-1]
-            continue
+        if rn == 'a' {
+            cnt++
+        } else {
+            cnt--
         }
-
-        if len(stack) > 0 && rn == 'a' && stack[len(stack)-1] == 'b' {
-            stack = stack[:len(stack)-1]
-            continue
-        }
-        stack = append(stack, rn)
     }
-    return len(stack)
+    return abs(cnt)
+}
+
+func abs(val int) int {
+    if val < 0 {
+        return -val
+    }
+    return val
 }
