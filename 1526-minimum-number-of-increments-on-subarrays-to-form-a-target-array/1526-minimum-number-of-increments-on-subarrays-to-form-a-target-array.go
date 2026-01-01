94e559ca6a43cfeebd29d54a7b3d9012a072212d
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-01-01 19:09:36 +0100

    Time: 0 ms (100%), Space: 10 MB (64.58%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.go
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.go
@@ -1,16 +1,8 @@
 func minNumberOperations(target []int) int {
-    stack := []int{}
-    ops := 0
-
-    for _, h := range target {
-        if len(stack) == 0 {
-            ops += h
-        } else if stack[len(stack)-1] < h {
-            ops += h - stack[len(stack)-1]
-        }
-
-        stack = append(stack, h)
-    }
-
-    return ops
+	n := len(target)
+	ans := target[0]
+	for i := 1; i < n; i++ {
+		ans += max(target[i]-target[i-1], 0)
+	}
+	return ans
 }
