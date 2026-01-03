a4891a618bedf2326db1b18dde9c6039c69575be
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-01-03 15:25:32 +0100

    Time: 1 ms (12.7%), Space: 4.2 MB (61.9%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.go
+++ 1544-make-the-string-great/1544-make-the-string-great.go
@@ -1,19 +1,12 @@
 func makeGood(s string) string {
-    //with the help of rune +/- 32 of a char gives the lower or upper case
-    // using a stack to keep track 
-    // if stack is empty push the cur and continue
-    // check the value of the char in the stack top if + 32 or -32 is equal to the next cur; pop the top and push in the next curr
-    // else push the next curr
     stack := []rune{}
     for _, rn := range s {
-        if len(stack) == 0 {
-            stack = append(stack, rn)
-            continue
-        }
-        top := stack[len(stack)-1]
-        if top + 32  == rn || top - 32 == rn {
-            stack = stack[:len(stack)-1]
-            continue
+        if len(stack) > 0 {
+            top := stack[len(stack)-1]
+            if top + 32  == rn || top - 32 == rn {
+                stack = stack[:len(stack)-1] // pop
+                continue
+            }
         }
         stack = append(stack, rn)
     }
