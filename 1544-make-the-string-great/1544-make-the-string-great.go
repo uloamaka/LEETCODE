95371844586452e605a45868d4faf50b3caccc31
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-01-03 15:26:31 +0100

    Time: 0 ms (100%), Space: 4.2 MB (61.9%) - LeetHub

--- 1544-make-the-string-great/1544-make-the-string-great.go
+++ 1544-make-the-string-great/1544-make-the-string-great.go
@@ -3,12 +3,19 @@ func makeGood(s string) string {
     for _, rn := range s {
         if len(stack) > 0 {
             top := stack[len(stack)-1]
-            if top + 32  == rn || top - 32 == rn {
+            if abs(int(top - rn)) == 32{
                 stack = stack[:len(stack)-1] // pop
-                continue
+                continue // skip current index
             }
         }
-        stack = append(stack, rn)
+        stack = append(stack, rn) // push
     }
     return string(stack)
+}
+
+func abs(n int) int {
+    if n < 0 {
+        return -n
+    }
+    return n
 }
