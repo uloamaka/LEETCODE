bcc06471876454fcc1112cf1e0560bc70ecf8182
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-01-04 16:49:02 +0100

    Time: 0 ms (100%), Space: 4 MB (75%) - LeetHub

--- 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.go
+++ 1190-reverse-substrings-between-each-pair-of-parentheses/1190-reverse-substrings-between-each-pair-of-parentheses.go
@@ -1,37 +1,18 @@
 func reverseParentheses(s string) string {
-    stack := []int{}
-    var str []string
-
-    for _, rn := range s {
-        if rn == '(' {
-            stack = append(stack, len(str))
-            continue
-        }
-        if rn == ')' {
-            top := stack[len(stack)-1]
-            stack = stack[:len(stack)-1]
-            rvsd := Reverse(str[top:])
-            str = append(str[:top], rvsd...)
-            continue
-        }
-        str = append(str, string(rn))
-    }
-    return Join(str)
+	stack := []byte{}
+	cur := []byte{}
+	for i := range s {
+		if s[i] != ')' {
+			stack = append(stack, s[i])
+		} else {
+			for stack[len(stack)-1] != '(' {
+				cur = append(cur, stack[len(stack)-1])
+				stack = stack[:len(stack)-1]
+			}
+			stack = stack[:len(stack)-1]
+			stack = append(stack, cur...)
+			cur = cur[:0]
+		}
+	}
+	return string(stack)
 }
-
-func Reverse(s []string) []string {
-    for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-        s[i], s[j] = s[j], s[i]
-    }
-    return s
-}
-
-
-func Join(slice []string) string {
-    var b strings.Builder
-    for _, s := range slice {
-        b.WriteString(s)
-    }
-    return b.String()
-}
-
