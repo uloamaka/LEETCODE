393d3da7de45a78b3debc8d3ab5705979a748eeb
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-01-08 02:09:00 +0100

    Time: 0 ms (100%), Space: 8.2 MB (31.9%) - LeetHub

---
+++ 0042-trapping-rain-water/0042-trapping-rain-water.go
@@ -0,0 +1,27 @@
+func trap(height []int) int {
+    stack := []int{}
+    water := 0 
+
+    for i := range height {
+        for len(stack) > 0 && height[i] > height[stack[len(stack)-1]] {
+            bottom := stack[len(stack)-1] // lowest height 
+            bottomHeight := height[bottom]
+            stack = stack[:len(stack)-1] // pop off from height index stack 
+
+            if len(stack) == 0 { // empty stack safety
+                break
+            }
+
+            leftWall := height[stack[len(stack)-1]] 
+            rightWall := height[i] 
+
+            width := i - stack[len(stack)-1] - 1
+            height := min(rightWall, leftWall) - bottomHeight
+            water += height * width
+        } 
+
+        stack = append(stack, i)
+    }
+    
+    return water
+}
