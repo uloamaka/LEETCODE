f7468eefab7cf7328dee021636bf889c7ffa9ed6
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-01-10 12:28:29 +0100

    Time: 0 ms (100%), Space: 8.5 MB (59.79%) - LeetHub

--- 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.go
+++ 2211-count-collisions-on-a-road/2211-count-collisions-on-a-road.go
@@ -1,40 +1,23 @@
 func countCollisions(directions string) int {
-	stack := []rune{}
-	collisions := 0
-
-	for _, car := range directions {
-		if car == 'R' {
-			stack = append(stack, 'R')
-			continue
-		}
-
-		if car == 'S' {
-			for len(stack) > 0 && stack[len(stack)-1] == 'R' {
-				stack = stack[:len(stack)-1]
-				collisions++
-			}
-			if len(stack) == 0 || stack[len(stack)-1] != 'S' {
-				stack = append(stack, 'S')
-			}
-			continue
-		}
-
-		if car == 'L' {
-			if len(stack) > 0 && stack[len(stack)-1] == 'R' {
-				stack = stack[:len(stack)-1]
-				collisions += 2
-
-				for len(stack) > 0 && stack[len(stack)-1] == 'R' {
-					stack = stack[:len(stack)-1]
-					collisions++
-				}
-				stack = append(stack, 'S')
-			} else if len(stack) > 0 && stack[len(stack)-1] == 'S' {
-				// L hits stopped car
-				collisions++
-			}
-		}
-	}
-
-	return collisions
+    n := len(directions)
+    l, r := 0, n-1
+    for l < n {
+        if directions[l] != 'L' {
+            break
+        }
+        l++
+    }
+    for r >= 0 {
+        if directions[r] != 'R' {
+            break
+        }
+        r--
+    }
+    total := 0
+    for i := l; i <= r; i++ {
+        if directions[i] != 'S' {
+            total++
+        }
+    }
+    return total
 }
