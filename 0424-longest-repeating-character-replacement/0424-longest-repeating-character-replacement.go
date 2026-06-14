95145f382960bfca5821b170c4281807aea335a7
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-06-14 10:32:51 +0100

    Time: 0 ms (100%), Space: 5.4 MB (86.21%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.go
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.go
@@ -1,17 +1,18 @@
 func characterReplacement(s string, k int) int {
-    var freq [256]int
+    var freq [26]int
     var maxChar int
     var maxFreq int 
     
     var l int
     for r := 0; r < len(s); r++ {
+        freq[s[r] - 'A']++
 
-        freq[s[r]]++
-
-        maxFreq = max(maxFreq, freq[s[r]])
+        if freq[s[r]- 'A'] > maxFreq {
+            maxFreq = freq[s[r] - 'A']
+        }
 
         if r - l + 1 - maxFreq  > k {
-            freq[s[l]]--
+            freq[s[l] - 'A']--
             l++
         }
 
