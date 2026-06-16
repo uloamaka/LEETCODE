5aa66820c60e1b5b8c897e255c66bfa5a07a51f9
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-06-16 14:58:50 +0100

    Time: 0 ms (100%), Space: 4.5 MB (54.47%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.go
+++ 0567-permutation-in-string/0567-permutation-in-string.go
@@ -2,23 +2,23 @@ func checkInclusion(s1 string, s2 string) bool {
     if len(s1) > len(s2) {
         return false
     }
-    var s1Slc [26]int
-    var s2Slc [26]int
+
+    var targetSlc, windowSlc [26]int
 
     for i := 0; i < len(s1); i++{
-        s1Slc[s1[i]-'a']++
-        s2Slc[s2[i]-'a']++
+        targetSlc[s1[i]-'a']++
+        windowSlc[s2[i]-'a']++
     }
 
-    if s1Slc == s2Slc {
+    if targetSlc == windowSlc {
         return true
     }
 
     for r := len(s1); r<len(s2); r++ {
-        s2Slc[s2[r]-'a']++
-        s2Slc[s2[r-len(s1)]-'a']--
+        windowSlc[s2[r]-'a']++
+        windowSlc[s2[r-len(s1)]-'a']--
 
-        if s1Slc == s2Slc {
+        if windowSlc == targetSlc {
             return true
         }
     }
