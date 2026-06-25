92707dfcf57c855c93fc4bcad554c5c5fc089d15
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-06-25 14:40:52 +0100

    Time: 7 ms (55.1%), Space: 4.6 MB (83.67%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.go
+++ 0016-3sum-closest/0016-3sum-closest.go
@@ -3,17 +3,18 @@ func threeSumClosest(nums []int, target int) int {
 
     closest := math.MaxInt
     ans := 0
-    for i := 0; i < len(nums); i++ {
-        if i > 0 && nums[i] == nums[i-1] {
-            continue
-        }
-
+    for i := 0; i < len(nums)-2; i++ {
+        
         l, r := i+1, len(nums)-1
         for l<r {
             sum := nums[i]+nums[l]+nums[r]
             diff := target - sum
             
             diffAbs := abs(diff)
+            if diffAbs == 0 {
+                return sum
+            }
+
             if diffAbs < closest {
                 closest = diffAbs
                 ans = sum
