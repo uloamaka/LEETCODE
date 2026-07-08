11bc348fc807ff52d1f17f4923c47e731aad57ed
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-07-08 23:26:47 +0100

    Time: 20 ms (71.58%), Space: 10.7 MB (86.31%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.go
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.go
@@ -7,18 +7,23 @@ func maxSlidingWindow(nums []int, k int) []int {
     deque := make([]int, 0)
 
     for i :=0; i<len(nums); i++ {
-
+        // Remove indices that are outside the current window
         if len(deque) > 0 && deque[0] <= i-k {
             deque = deque[1:]
         }
 
+        // Remove all smaller (or equal) values from the back
+		// because they can never become the maximum
         for len(deque) > 0 && nums[deque[len(deque)-1]] <= nums[i] {
             deque = deque[:len(deque)-1]
         }
 
+        // add the current index
         deque = append(deque, i)
 
-        if i+1 >= k {
+        // Once we've formed the first full window,
+		// record the maximum (front of deque)
+        if i >= k-1 {
             result = append(result, nums[deque[0]])
         }
     }
