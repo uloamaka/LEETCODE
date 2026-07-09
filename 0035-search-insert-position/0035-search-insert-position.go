426ef142ffd90f649ebe676655cd770642d5dbc0
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-07-09 23:20:00 +0100

    Time: 0 ms (100%), Space: 4.8 MB (20.72%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.go
+++ 0035-search-insert-position/0035-search-insert-position.go
@@ -1,18 +1,14 @@
 func searchInsert(nums []int, target int) int {
-    left, right, lastChk := 0, len(nums), 0
+    left, right := 0, len(nums)
 
     for left < right {
         mid := left + (right - left) / 2
 
-        if nums[mid] == target {
-            return mid
-        } else if nums[mid] < target {
+        if nums[mid] < target {
             left = mid + 1
-            lastChk = mid+1
         } else {
             right = mid
-            lastChk = mid
         }
     }
-    return lastChk
+    return left
 }
