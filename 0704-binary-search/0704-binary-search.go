a3cd96ab9c0a285f6f783e2699d23b4ff5782620
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-07-11 23:51:52 +0100

    Time: 0 ms (100%), Space: 8.4 MB (97.52%) - LeetHub

--- 0704-binary-search/0704-binary-search.go
+++ 0704-binary-search/0704-binary-search.go
@@ -1,28 +1,17 @@
 func search(nums []int, target int) int {
-    left := 0
-    right := len(nums) 
+    left, right := 0, len(nums)
 
     for left < right {
-        mid := left + (right - left) / 2
-        if nums[mid] <= target {
+        mid := left + (right-left) / 2
+
+        if nums[mid] == target {
+            return mid
+        } else if nums[mid] < target {
             left = mid + 1
         } else {
             right = mid
-        } 
-    } 
-    if left > 0 && nums[left - 1] == target{
-        return  left - 1
-    }  
-    return -1 
-}
+        }
+    }
 
-    // for left <= right {
-    //     mid := left + (right - left) / 2
-    //     if nums[mid] == target {
-    //         return mid
-    //     } else if nums[mid] < target {
-    //         left = mid + 1
-    //     } else {
-    //         right = mid - 1
-    //     }
-    // }
+    return -1
+}
