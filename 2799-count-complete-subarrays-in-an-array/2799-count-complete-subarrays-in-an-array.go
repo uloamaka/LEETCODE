a8a981317960ad86d1c9863aeef995d0ee060b8c
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-07-12 20:47:26 +0100

    Time: 13 ms (25%), Space: 8.3 MB (75%) - LeetHub

--- 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.go
+++ 2799-count-complete-subarrays-in-an-array/2799-count-complete-subarrays-in-an-array.go
@@ -1,14 +1,16 @@
 func countCompleteSubarrays(nums []int) int {
     freq := make(map[int]int)
-    cnt := 0
     for _, num := range nums {
         freq[num]++
     }
 
-    seen := make(map[int]int) 
-    left := 0
+    seen := make(map[int]int)
+
+    left:= 0
+    cnt := 0
     for right := 0; right < len(nums); right++ {
         seen[nums[right]]++
+
         for len(seen) == len(freq) {
             seen[nums[left]]--
 
@@ -17,9 +19,9 @@ func countCompleteSubarrays(nums []int) int {
             }
 
             left++
-            cnt += len(nums)-right
         }
-    }
 
+        cnt += left
+    }
     return cnt
 }
