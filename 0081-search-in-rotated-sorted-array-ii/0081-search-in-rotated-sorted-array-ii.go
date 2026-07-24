faec4eb32aabf371d89055a01d18739f7c66dfcb
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-07-24 07:51:59 +0100

    Time: 0 ms (100%), Space: 5 MB (19.65%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.go
@@ -1,33 +1,34 @@
 func search(nums []int, target int) bool {
-    // left, right := 0, len(nums)-1
+    left, right := 0, len(nums)-1
 
-    // for left < right {
-    //     mid := left + (right-left) / 2
+    for left <= right {
+        mid := left + (right-left) / 2
 
-    //     if nums[mid] == target {
-    //         return true
-    //     }
-
-    //     // [left, mid] is sorted
-    //     if nums[left] <= nums[mid] {
-    //         if nums[left] <= target && target < nums[mid] {
-    //             right = mid - 1
-    //         } else {
-    //             left = mid + 1
-    //         }
-    //     } else {
-    //         if nums[mid] < target && target <= nums[right] {
-    //             left = mid + 1
-    //         } else {
-    //             right = mid - 1
-    //         }
-    //     }
-    // }
-
-    for i := 0; i < len(nums); i++ {
-        if nums[i] == target {
+        if nums[mid] == target {
             return true
         }
+
+        // Cannot determine which half is sorted.
+        if nums[left] == nums[mid] && nums[mid] == nums[right] {
+            left++
+            right--
+            continue
+        }
+
+        if nums[left] <= nums[mid] {
+            if nums[left] <= target && target < nums[mid] {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        } else {
+            if nums[mid] < target && target <= nums[right] {
+                left = mid + 1
+            } else {
+                right = mid - 1
+            }
+        }
     }
+
     return false
 }
