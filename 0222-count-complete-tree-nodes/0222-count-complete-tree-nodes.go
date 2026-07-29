d765a7e9c1aab89af6ee4a0397fb7e412ac689df
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-07-29 04:29:47 +0100

    Time: 0 ms (100%), Space: 9.2 MB (59.55%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.go
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.go
@@ -6,10 +6,38 @@
  *     Right *TreeNode
  * }
  */
+func findLeftHeight(root *TreeNode) int {
+    count := 0
+    for root != nil {
+        root = root.Left
+        count++
+    }
+    return count
+}
+
+func findRightHeight(root *TreeNode) int {
+    count := 0
+    for root != nil {
+        root = root.Right
+        count++
+    }
+    return count
+}
+
 func countNodes(root *TreeNode) int {
     if root == nil {
         return 0
     }
-    // 1 (current node) + count of left branch + count of right branch
+    
+    l := findLeftHeight(root)
+    r := findRightHeight(root)
+    
+    // If the left and right depths are equal, it's a perfect binary tree.
+    if l == r {
+        // (1 << l) is equivalent to 2^l. 
+        return (1 << l) - 1 
+    }
+    
+    // Otherwise, recursively count the left and right subtrees.
     return 1 + countNodes(root.Left) + countNodes(root.Right)
 }
