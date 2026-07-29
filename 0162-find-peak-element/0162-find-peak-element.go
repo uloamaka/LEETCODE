4fe8122bec78ba884e2ad051c9d0a338df9969ec
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-07-29 20:41:45 +0100

    Time: 0 ms (100%), Space: 4.6 MB (46.45%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.go
+++ 0162-find-peak-element/0162-find-peak-element.go
@@ -1,31 +1,15 @@
 func findPeakElement(nums []int) int {
-    n := len(nums)
-    if n == 1 {
-        return 0
-    }
+    left, right := 0, len(nums)-1
 
-    if nums[0] > nums[1] {
-        return 0
-    }
+    for left < right {
+        mid := left + (right-left)/2
 
-    if nums[n-1] > nums[n-2] {
-        return n - 1
-    }
-
-    left, right := 1, len(nums)-2
-    for left <= right {
-        mid := left + (right - left) / 2
-
-        if nums[mid-1] < nums[mid] && nums[mid] > nums[mid+1] {
-            return mid
-        } 
-
-        if nums[mid] < nums[mid+1] {
-            left = mid + 1
+        if nums[mid] > nums[mid+1] {
+            right = mid
         } else {
-            right = mid - 1
+            left = mid + 1
         }
     }
-   
-    return -1
+
+    return left
 }
