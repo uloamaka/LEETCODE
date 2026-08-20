39ff0c77bfff92d2bd3555b10e0bc2657dc37aac
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-08-20 03:40:16 +0100

    Time: 0 ms (100%), Space: 4.9 MB (0.33%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.go
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.go
@@ -1,16 +1,31 @@
-func runningSum(nums []int) []int {
-    ans := []int{}
+// func runningSum(nums []int) []int {
+//     ans := []int{}
 
-    for i := 0; i < len(nums); i++ {
-        ans = append(ans, recur(nums, i))
-    }
+//     for i := 0; i < len(nums); i++ {
+//         ans = append(ans, recur(nums, i))
+//     }
    
-    return ans
+//     return ans
+// }
+
+// func recur(nums []int, n int) int {
+//     if n == 0 {
+//         return nums[0]
+//     }
+//     return nums[n] + recur(nums, n-1)
+// }
+
+func recur(nums []int, n int, sum int, ans []int) []int {
+    if n == len(nums) {
+        return ans
+    }
+
+    sum += nums[n]
+    ans = append(ans, sum)
+
+    return recur(nums, n+1, sum, ans)
 }
 
-func recur(nums []int, n int) int {
-    if n == 0 {
-        return nums[0]
-    }
-    return nums[n] + recur(nums, n-1)
+func runningSum(nums []int) []int {
+    return recur(nums, 0, 0, []int{})
 }
