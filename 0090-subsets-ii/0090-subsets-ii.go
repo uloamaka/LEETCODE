f455eb3b27443c5d0616ac570e2d7a9a269c747a
Godsgift Uloamaka Ebite <godsgiftuloamaka235@gmail.com>
2026-08-22 18:35:33 +0100

    Time: 0 ms (100%), Space: 4.5 MB (18.57%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.go
+++ 0090-subsets-ii/0090-subsets-ii.go
@@ -1,66 +1,29 @@
 func subsetsWithDup(nums []int) [][]int {
     sort.Ints(nums)
-    subset := []int{}
-    return subsetFinder(nums, 0, subset)
-}
-
-func subsetFinder(nums []int, idx int, subset []int) [][]int {
-    n := len(nums)
     ans := [][]int{}
+    subset := []int{}
 
-    if idx == n {
-        copySubset := append([]int{}, subset...)
-        ans = append(ans, copySubset)
-        return ans
+    var dfs func(i int, subset []int)
+
+    dfs = func(i int, subset []int) {
+        n := len(nums)
+        if i == n {
+            copySubset := append([]int{}, subset...)
+            ans = append(ans, copySubset)
+            return 
+        }
+
+        subset = append(subset, nums[i])
+        dfs(i+1, subset)
+
+        subset = subset[:len(subset)-1]
+        for i < n-1 && nums[i] == nums[i+1] {
+            i++
+        }
+        dfs(i+1, subset)
     }
 
-    subset = append(subset, nums[idx])
-    ans = append(ans, subsetFinder(nums, idx+1, subset)...)
+    dfs(0, subset)
 
-    subset = subset[:len(subset)-1]
-    for idx < n-1 && nums[idx] == nums[idx+1] {
-        idx++
-    }
-    ans = append(ans, subsetFinder(nums, idx+1, subset)...)
-
-    return ans
+    return ans 
 }
-/////////////////////////////////////////////////////////
-/* func subsetsWithDup(nums []int) [][]int {
-    sort.Ints(nums)
-    var queue [][]int
-    queue = append(queue, []int{})
-    
-    m := make(map[int]int)
-    
-    for i := 0; i < len(nums); i++ {
-        length := len(queue)
-        
-        limit := -1
-        
-        if val, ok := m[nums[i]]; ok {
-            limit = val
-        } else {
-            m[nums[i]] = length
-        }
-        
-        m[nums[i]] = length
-        
-        for j := 0; j < length; j++ {
-            if limit != -1 && j < limit {
-                continue    
-            }
-            
-            item := queue[j]
-
-            tmp := make([]int, len(item))
-            copy(tmp, item)
-            tmp = append(tmp, nums[i])
-
-            queue = append(queue, tmp) 
-        }
-    }
-    
-    
-    return queue
-} */
